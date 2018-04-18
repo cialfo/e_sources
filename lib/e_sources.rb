21adfabfe2b3a0666d914ae29b26354e99f84396
uzair <syeduzairahmad@live.com>
2018-04-18 12:38:30 +0800

    changed version

--- lib/e_sources.rb
+++ lib/e_sources.rb
@@ -1,6 +1,9 @@
-# require "e_sources/version"
+require 'e_sources/client'
 require 'e_sources/cryptographer'
-
+require 'e_sources/exceptions'
+require 'e_sources/hes_client'
+require 'e_sources/types'
+require 'e_sources/version'
 module ESources
   # Your code goes here...
 end
